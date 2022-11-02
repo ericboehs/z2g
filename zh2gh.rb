a3e8704cdc8e3a8523d3f01bd31e600c34da97c6
Eric Boehs <eric@boehs.com>
2022-11-02 17:19:40 -0500

    Convert config to ENV vars

--- zh2gh.rb
+++ zh2gh.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift 'lib'
+require 'dotenv'
 require 'github/project'
 require 'zenhub/workspace'
 require 'syncer'
@@ -10,22 +11,19 @@ require 'zenhub_ruby'
 
 project = Github::Project.new(
   token: ENV.fetch('GH_TOKEN'),
-  organization: 'department-of-veterans-affairs',
-  repo: 'va.gov-team',
-  number: 807,
-  label: 'platform-tech-team-3'
+  organization: ENV.fetch('Z2G_ORGANIZATION'),
+  repo: ENV.fetch('Z2G_REPOSITORY'),
+  number: ENV.fetch('Z2G_PROJECT'),
+  label: ENV.fetch('Z2G_LABEL')
 )
 
 workspace = Zenhub::Workspace.new(
   token: ENV.fetch('ZH_TOKEN'),
   github_token: ENV.fetch('GH_TOKEN'),
-  organization: 'department-of-veterans-affairs',
-  repo: 'va.gov-team',
-  id: '6335ab9b1901b99243ce7601',
-  column_names_map: {
-    'Backlog' => ['Current Sprint', 'Upcoming Sprint', 'Backlog', 'Current Initiatives/Epics', 'Icebox'],
-    'In Progress' => ['Blocked/Has Dependency', 'Review/QA', 'In Progress']
-  },
+  organization: ENV.fetch('Z2G_ORGANIZATION'),
+  repo: ENV.fetch('Z2G_REPOSITORY'),
+  id: ENV.fetch('Z2G_WORKSPACE_ID'),
+  column_names_map: JSON.parse(ENV.fetch('Z2G_COLUMN_NAMES_MAP')),
   issue_numbers: project.issues_for_label.map { |issue| issue[:number] }
 )
 
